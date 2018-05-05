ce1db19a85df16fdc151c457be3a5adb6440109c
Jack D <dmarkov23@gmail.com>
2018-05-05 17:32:10 +0200

    setup of automata class structure

---
+++ src/main/java/practical/automata/calculations/structures/Automata.java
@@ -0,0 +1,43 @@
+package practical.automata.calculations.structures;
+
+import java.util.List;
+import java.util.Map;
+
+public class Automata {
+    private String alphabet;
+    private List<String> states;
+    private List<String> final_states;
+    private Map<String, List<String>> transitions;
+
+    public String getAlphabet() {
+        return alphabet;
+    }
+
+    public void setAlphabet(String alphabet) {
+        this.alphabet = alphabet;
+    }
+
+    public List<String> getStates() {
+        return states;
+    }
+
+    public void setStates(List<String> states) {
+        this.states = states;
+    }
+
+    public List<String> getFinalStates() {
+        return final_states;
+    }
+
+    public void setFinalStates(List<String> final_states) {
+        this.final_states = final_states;
+    }
+
+    public Map<String, List<String>> getTransitions() {
+        return transitions;
+    }
+
+    public void setTransitions(Map<String, List<String>> transitions) {
+        this.transitions = transitions;
+    }
+}
