09a6238bd08d21de59c46ab052f57627f7766bdc
Jack D <dmarkov23@gmail.com>
2018-05-05 17:12:17 +0200

    created method for converting file into a list of strings

--- src/main/java/practical/automata/calculations/utils/AutomataFileReader.java
+++ src/main/java/practical/automata/calculations/utils/AutomataFileReader.java
@@ -1,10 +1,36 @@
 package practical.automata.calculations.utils;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class AutomataFileReader {
 
     public void readAutomataFile() {
-        
+        List<String> fileLines = generateListWithFileLines();
 
+
+    }
+
+    private List<String> generateListWithFileLines() {
+
+        String FILE_PATH = "./out/production/resources/dot-files/graph.dot";
+
+        Scanner sc = null;
+        try {
+            sc = new Scanner(new File(FILE_PATH));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        List<String> lines = new ArrayList<String>();
+        while (sc.hasNextLine()) {
+            lines.add(sc.nextLine());
+        }
+        sc.close();
+
+        return lines;
     }
 
 }
