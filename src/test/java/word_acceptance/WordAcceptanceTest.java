20e22abd905d1b966d1d0b0260df7d1dff7deed8
Jack D <dmarkov23@gmail.com>
2018-07-05 20:14:48 +0200

    created some tests for word acceptance

---
+++ src/test/java/word_acceptance/WordAcceptanceTest.java
@@ -0,0 +1,32 @@
+package word_acceptance;
+
+import org.junit.Test;
+import practical.automata.calculations.CalculationsManager;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class WordAcceptanceTest {
+    private CalculationsManager calculationsManager = new CalculationsManager(null);
+
+    @Test
+    public void areWordsAcceptedTestFile1() {
+        String testWordThatShouldBeAccepted = "baab";
+        String testWordWithLetterThatDoesNotExistInTheAlphabet = "gaba";
+        String testWordThatShouldNotBeAccepted = "baba";
+
+        boolean isAccepted;
+        // Set the file we want to test with
+        calculationsManager.InitializeValuesFromFile("./src/test/resources/word_acceptance/test-file1.txt");
+
+        isAccepted = calculationsManager.isWordAccepted(testWordThatShouldBeAccepted);
+        assertTrue(isAccepted);
+
+        isAccepted = calculationsManager.isWordAccepted(testWordWithLetterThatDoesNotExistInTheAlphabet);
+        assertFalse(isAccepted);
+
+        isAccepted = calculationsManager.isWordAccepted(testWordThatShouldNotBeAccepted);
+        assertFalse(isAccepted);
+
+    }
+}
