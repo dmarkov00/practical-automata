9a9fc4ac8b74055b6620e50057da47d265044440
Jack D <dmarkov23@gmail.com>
2018-07-05 16:48:09 +0200

    created some tests for dfa indicating

--- src/test/java/indicate_dfa/DfaIndicatorTest.java
+++ src/test/java/indicate_dfa/DfaIndicatorTest.java
@@ -7,20 +7,43 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 /**
- *
+ * This test files are manually checked and they are legitimate
  */
 public class DfaIndicatorTest {
 
     // Pass null for file path for now
-    CalculationsManager calculationsManager = new CalculationsManager(null);
+    private CalculationsManager calculationsManager = new CalculationsManager(null);
+
+
+    @Test
+    public void checkForDfaWithDfaTestFile() {
+        // Set the file we want to test with
+        calculationsManager.InitializeValuesFromFile("./src/test/resources/indicate_dfa/dfa-state-machine.txt");
+
+        boolean isDFA = calculationsManager.indicateDFA();
+
+        assertTrue(isDFA);
+    }
 
     /**
-     * This test file is manually checked and it is not a DFA
+     * If a file has an empty transition it should be false
      */
     @Test
-    public void checkForDfaWithTestFile1() {
+    public void checkForDfaWithNdfaWithEmptyTransitionsTestFile() {
         // Set the file we want to test with
-        calculationsManager.InitializeValuesFromFile("./src/test/resources/test.txt");
+        calculationsManager.InitializeValuesFromFile("./src/test/resources/indicate_dfa/ndfa-state-machine-with-empty-transition.txt");
+
+        boolean isDFA = calculationsManager.indicateDFA();
+
+        assertFalse(isDFA);
+    }
+    /**
+     * The file is not using all transitions symbols from all states
+     */
+    @Test
+    public void checkForDfaWithNdfaTestFile() {
+        // Set the file we want to test with
+        calculationsManager.InitializeValuesFromFile("./src/test/resources/indicate_dfa/ndfa-state-machine.txt");
 
         boolean isDFA = calculationsManager.indicateDFA();
 
