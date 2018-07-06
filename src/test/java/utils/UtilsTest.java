bbb3fd476920b4f1e993d1e5126b4ab2cc325c11
Jack D <dmarkov23@gmail.com>
2018-07-06 05:52:06 +0200

    added some test for unique state generation

--- src/test/java/utils/UtilsTest.java
+++ src/test/java/utils/UtilsTest.java
@@ -3,18 +3,36 @@ package utils;
 import org.junit.Test;
 import practical.automata.calculations.utils.Utils;
 
+import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 
 public class UtilsTest {
 
     @Test
-    public void generateUniqueStatesTest() {
+    public void generateUniqueStatesForTheTenthCallOfTheMethod() {
 
+        String stateName = null;
         for (int i = 0; i < 10; i++) {
-        String a = Utils.generateUniqueState();
+            stateName = Utils.generateUniqueState();
 
         }
 
-    assertThat()
+        assertThat(stateName, is("J"));
+    }
+
+    /**
+     *  Expected B2 because I am using 24 letters of the alphabet (without R and T) -> i.e. A,B,C...A1,B1....,A2,B2
+     */
+
+    @Test
+    public void generateUniqueStatesForTheFiftiethCallOfTheMethod() {
+
+        String stateName = null;
+        for (int i = 0; i < 50; i++) {
+            stateName = Utils.generateUniqueState();
+
+        }
+
+        assertThat(stateName, is("B2"));
     }
 }
